e93003335cbf0af90a5163e701cd6bd0ea49a33b
andrescarlos2211 <andrescarlos2211@gmail.com>
2023-01-10 15:54:00 -0300

    Primer Commit Crea ScriptJs

---
+++ app.js
@@ -0,0 +1,18 @@
+let regionesChile = [
+   {imagenRegion:1, Región: 'Región de Arica y Parinacota', Atractivos: '1. Morro de Arica · 2. Parque Nacional Lauca · 3. Museo Arqueologico San Miguel de Azapa' }, 
+   {imagenRegion:2 , Región: 'Región de Tarapacá', Atractivos: '1. Oficina Salitrera Santiago Humberstone · 2. Museo Corbeta Esmeralda · 3. Playa Cavancha' },   
+   {imagenRegion:3, Región: 'Región de Antofagasta', Atractivos: '1. Salar de Atacama · 2. La Portada · 3. Ruinas de Huanchaca' },   
+   {imagenRegion:4, Región: 'Región de Atacama', Atractivos: '1. Playa la virgen · 2. Bahía Inglesa · 3. Desierto Florido' },   
+   {imagenRegion:5, Región: 'Región de Coquimbo', Atractivos: '1. Cruz Del Tercer Milenio · 2. Playa La Herradura · 3. Totoralillo' },   
+   {imagenRegion:6, Región: 'Región de Valparaíso', Atractivos: '1. La Sebastiana · 2. Cerro Concepción · 3. Paseo Gervasoni' },   
+   {imagenRegion:7, Región: 'Región Metropolitana de Santiago', Atractivos: '1. Cerro Santa Lucía · 2. Barrio Lastarria · 3. Museo de la Memoria y los Derechos Humanos' },   
+   {imagenRegion:8, Región: 'Región del Libertador General Bernardo OHiggins', Atractivos: '1. Colchagua Museum · 2. MontGras · 3. Lapostolle Clos Apalta'},
+   {imagenRegion:9, Región: 'Región del Maule', Atractivos: '1. Parque Nacional Radal Siete Tazas · 2. Reserva Nacional Altos de Lircay · 3. Piedra de la Iglesia' },   
+   {imagenRegion:10, Región: 'Región de Ñuble', Atractivos: '1. Termas de Chillán · 2. Portezuelo · 3. Longanizas Iturra: las ganadoras de este año' },   
+   {imagenRegion:11, Región: 'Región del Biobío', Atractivos: '1. Universidad de Concepcion · 2. Parque Ecuador · 3. Monitor Huáscar'},   
+   {imagenRegion:12, Región: 'Región de La Araucanía', Atractivos: '1. Termas Geometricas · 2. Volcan Villarrica · 3. Parque Nacional Huerquehue' },   
+   {imagenRegion:13, Región: 'Región de Los Ríos', Atractivos: '1. Saltos del Huilo-Huilo · 2. Parque Oncol · 3. Mercado Fluvial de Vrealdivia' },   
+   {imagenRegion:14, Región: 'Región de Los Lagos', Atractivos: '1. Saltos del Petrohué · 2. Lago Llanquihue · 3. Lago Todos Los Santos ' },   
+   {imagenRegion:15, Región: 'Región de Aysén del General Carlos Ibáñez del Campo', Atractivos: '1. Salto Río Ibañez · 2. Embarcadero Puerto Ibañez · 3. Huellas Pioneras' },   
+   {imagenRegion:16, Región: 'Región de Magallanes y de la Antártica Chilena', Atractivos: '1. Parque Nacional Torres del Paine · 2. Glacier Grey · 3. Cementerio Municipal'}
+]
