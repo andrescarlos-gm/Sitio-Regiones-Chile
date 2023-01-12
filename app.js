0dc6e7e5850ba4d7b472ac472a47b40d13d1c5c9
andrescarlos2211 <andrescarlos2211@gmail.com>
2023-01-12 17:02:08 -0300

    fotos

--- app.js
+++ app.js
@@ -1,89 +1,91 @@
-let regionesChile = [
+const regionesChile = [
    {
       imagenRegion:'img/1.jpg/', 
-      Región: 'Región de Arica y Parinacota', 
-      Atractivos: '1. Morro de Arica · 2. Parque Nacional Lauca · 3. Museo Arqueologico San Miguel de Azapa' 
+      Region: 'Región de Arica y Parinacota', 
+      atractivos: '1. Morro de Arica · 2. Parque Nacional Lauca · 3. Museo Arqueologico San Miguel de Azapa' 
    },
    {
       imagenRegion:'img/2.jpg/', 
-      Región: 'Región de Tarapacá', 
-      Atractivos: '1. Oficina Salitrera Santiago Humberstone · 2. Museo Corbeta Esmeralda · 3. Playa Cavancha' 
+      Region: 'Región de Tarapacá', 
+      atractivos: '1. Oficina Salitrera Santiago Humberstone · 2. Museo Corbeta Esmeralda · 3. Playa Cavancha' 
    },   
    {
       imagenRegion:'img/3.jpg/', 
-      Región: 'Región de Antofagasta', 
-      Atractivos: '1. Salar de Atacama · 2. La Portada · 3. Ruinas de Huanchaca' 
+      Region: 'Región de Antofagasta', 
+      atractivos: '1. Salar de Atacama · 2. La Portada · 3. Ruinas de Huanchaca' 
    },   
    {
       imagenRegion:'img/4.jpg/', 
-      Región: 'Región de Atacama', 
-      Atractivos: '1. Playa la virgen · 2. Bahía Inglesa · 3. Desierto Florido' 
+      Region: 'Región de Atacama', 
+      atractivos: '1. Playa la virgen · 2. Bahía Inglesa · 3. Desierto Florido' 
    },   
    {
       imagenRegion:'img/5.jpg/', 
-      Región: 'Región de Coquimbo', 
-      Atractivos: '1. Cruz Del Tercer Milenio · 2. Playa La Herradura · 3. Totoralillo' 
+      Region: 'Región de Coquimbo', 
+      atractivos: '1. Cruz Del Tercer Milenio · 2. Playa La Herradura · 3. Totoralillo' 
    },   
    {
       imagenRegion:'img/6.jpg/', 
-      Región: 'Región de Valparaíso', 
-      Atractivos: '1. La Sebastiana · 2. Cerro Concepción · 3. Paseo Gervasoni' 
+      Region: 'Región de Valparaíso', 
+      atractivos: '1. La Sebastiana · 2. Cerro Concepción · 3. Paseo Gervasoni' 
    },   
    {
       imagenRegion:'img/7.jpg/', 
-      Región: 'Región Metropolitana de Santiago', 
-      Atractivos: '1. Cerro Santa Lucía · 2. Barrio Lastarria · 3. Museo de la Memoria y los Derechos Humanos' 
+      Region: 'Región Metropolitana de Santiago', 
+      atractivos: '1. Cerro Santa Lucía · 2. Barrio Lastarria · 3. Museo de la Memoria y los Derechos Humanos' 
    },   
    {
       imagenRegion:'img/8.jpg/', 
-      Región: 'Región del Libertador General Bernardo OHiggins', 
-      Atractivos: '1. Colchagua Museum · 2. MontGras · 3. Lapostolle Clos Apalta'
+      Region: 'Región del Libertador General Bernardo OHiggins', 
+      atractivos: '1. Colchagua Museum · 2. MontGras · 3. Lapostolle Clos Apalta'
    },
    {
       imagenRegion:'img/9.jpg/', 
-      Región: 'Región del Maule', 
-      Atractivos: '1. Parque Nacional Radal Siete Tazas · 2. Reserva Nacional Altos de Lircay · 3. Piedra de la Iglesia' 
+      Region: 'Región del Maule', 
+      atractivos: '1. Parque Nacional Radal Siete Tazas · 2. Reserva Nacional Altos de Lircay · 3. Piedra de la Iglesia' 
    },   
    {
       imagenRegion:'img/10.jpg/', 
-      Región: 'Región de Ñuble', 
-      Atractivos: '1. Termas de Chillán · 2. Portezuelo · 3. Longanizas Iturra: las ganadoras de este año' 
+      Region: 'Región de Ñuble', 
+      atractivos: '1. Termas de Chillán · 2. Portezuelo · 3. Longanizas Iturra: las ganadoras de este año' 
    },   
    {
       imagenRegion:'img/11.jpg/', 
-      Región: 'Región del Biobío', 
-      Atractivos: '1. Universidad de Concepcion · 2. Parque Ecuador · 3. Monitor Huáscar'
+      Region: 'Región del Biobío', 
+      atractivos: '1. Universidad de Concepcion · 2. Parque Ecuador · 3. Monitor Huáscar'
    },   
    {
       imagenRegion:'img/12.jpg/', 
-      Región: 'Región de La Araucanía', 
-      Atractivos: '1. Termas Geometricas · 2. Volcan Villarrica · 3. Parque Nacional Huerquehue' 
+      Region: 'Región de La Araucanía', 
+      atractivos: '1. Termas Geometricas · 2. Volcan Villarrica · 3. Parque Nacional Huerquehue' 
    },   
    {
       imagenRegion:'img/13.jpg/', 
-      Región: 'Región de Los Ríos', 
-      Atractivos: '1. Saltos del Huilo-Huilo · 2. Parque Oncol · 3. Mercado Fluvial de Vrealdivia' 
+      Region: 'Región de Los Ríos', 
+      atractivos: '1. Saltos del Huilo-Huilo · 2. Parque Oncol · 3. Mercado Fluvial de Vrealdivia' 
    },   
    {
       imagenRegion:'img/14.jpg/', 
-      Región: 'Región de Los Lagos', 
-      Atractivos: '1. Saltos del Petrohué · 2. Lago Llanquihue · 3. Lago Todos Los Santos ' 
+      Region: 'Región de Los Lagos', 
+      atractivos: '1. Saltos del Petrohué · 2. Lago Llanquihue · 3. Lago Todos Los Santos ' 
    },   
    {
       imagenRegion:'img/15.jpg/', 
-      Región: 'Región de Aysén del General Carlos Ibáñez del Campo', 
-      Atractivos: '1. Salto Río Ibañez · 2. Embarcadero Puerto Ibañez · 3. Huellas Pioneras' 
+      Region: 'Región de Aysén del General Carlos Ibáñez del Campo', 
+      atractivos: '1. Salto Río Ibañez · 2. Embarcadero Puerto Ibañez · 3. Huellas Pioneras' 
    },   
    {
       imagenRegion:'img/16.jpg/', 
-      Región: 'Región de Magallanes y de la Antártica Chilena', 
-      Atractivos: '1. Parque Nacional Torres del Paine · 2. Glacier Grey · 3. Cementerio Municipal'
+      Region: 'Región de Magallanes y de la Antártica Chilena', 
+      atractivos: '1. Parque Nacional Torres del Paine · 2. Glacier Grey · 3. Cementerio Municipal'
    }
 ]
-$("document").ready(function(){
+// $("document").ready(function(){
 
 
 
-   $("mapa").hover(function(){ /*es necesario declararla como id?*/ 
-      $("cards").toogle();
-   })})
+//    $("mapa").hover(function(){ /*es necesario declararla como id?*/ 
+//       $("cards").toogle();
+//    })})
+
+
