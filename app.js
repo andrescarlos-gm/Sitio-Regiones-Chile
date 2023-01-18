f0d04da9a7367c11122b9546a9f28e423c4b1dbe
andrescarlos2211 <andrescarlos2211@gmail.com>
2023-01-18 13:49:59 -0300

    corrige error

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var array1 = [["Región de Arica y Parinacota", "img/1.jpg", "1. Morro de Arica 
 ["Región de Atacama", "img/4.jpg", "1. Playa la virgen · 2. Bahía Inglesa · 3. Desierto Florido"],
 ["Región de Coquimbo", "img/5.jpg", "1. Cruz Del Tercer Milenio · 2. Playa La Herradura · 3. Totoralillo"],
 ["Región de Valparaíso", "img/6.jpg", "1. La Sebastiana · 2. Cerro Concepción · 3. Paseo Gervasoni"],
-["Región Metropolitana de Santiago", "img/7.jpg", "1. Cerro Santa Lucía · 2. Barrio Lastarria · 3. Museo de la Memoria y los Derechos Humanos"],
+["Región Metropolitana de Santiago", "img/7.jpg", "1. Cerro Santa Lucía · 2. Universidad de Chile · 3. Museo de la Memoria y los Derechos Humanos"],
 ["Región del Libertador General Bernardo OHiggins", "img/8.jpg", "1. Colchagua Museum · 2. MontGras · 3. Lapostolle Clos Apalta"],
 ["Región del Maule", "img/9.jpg", "1. Parque Nacional Radal Siete Tazas · 2. Reserva Nacional Altos de Lircay · 3. Piedra de la Iglesia"],
 ["Región de Ñuble", "img/10.jpg", "1. Termas de Chillán · 2. Portezuelo · 3. Longanizas Iturra: las ganadoras de este año"],
 ["Región del Biobío", "img/11.jpg", "1. Universidad de Concepcion · 2. Parque Ecuador · 3. Monitor Huáscar"],
 ["Región de La Araucanía", "img/12.jpg", "1. Termas Geometricas · 2. Volcan Villarrica · 3. Parque Nacional Huerquehue"],
 ["Región de Los Ríos", "img/13.jpg", "1. Saltos del Huilo-Huilo · 2. Parque Oncol · 3. Mercado Fluvial de Valdivia"],
-["Región de Los Lagos", "img/14.jpg", "1. Saltos del Petrohué · 2. Lago Llanquihue · 3. Lago Todos Los Santos "], +
+["Región de Los Lagos", "img/14.jpg", "1. Saltos del Petrohué · 2. Lago Llanquihue · 3. Lago Todos Los Santos "],
 ["Región de Aysén del General Carlos Ibáñez del Campo", "img/15.jpg", "1. Salto Río Ibañez · 2. Embarcadero Puerto Ibañez · 3. Huellas Pioneras"],
 ["Región de Magallanes y de la Antártica Chilena", "img/16.jpg", "1. Parque Nacional Torres del Paine · 2. Glacier Grey · 3. Cementerio Municipal"]]
 
@@ -104,17 +104,15 @@ function tarjetaRegion(valor) {
          atractivos.innerHTML = array1[13][2];
          break;
       case 15:
-         titulo.innerText = array1[13][0];
-         document.getElementById("imagenRegion").src = array1[13][1];
-         atractivos.innerHTML = array1[13][2];
+         titulo.innerText = array1[14][0];
+         document.getElementById("imagenRegion").src = array1[14][1];
+         atractivos.innerHTML = array1[14][2];
          break;
       case 16:
-         titulo.innerText = array1[13][0];
-         document.getElementById("imagenRegion").src = array1[13][1];
-         atractivos.innerHTML = array1[13][2];
+         titulo.innerText = array1[15][0];
+         document.getElementById("imagenRegion").src = array1[15][1];
+         atractivos.innerHTML = array1[15][2];
          break;
-
-
    }
 }
 
